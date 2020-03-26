31c06853ce20fd87cb658187e534d1c34561c978
thdann <theresa_dannberg@hotmail.com>
2020-03-26 15:11:25 +0100

    Uppdateringar i Serverkoden

--- Server/src/Controller/ServerController.java
+++ Server/src/Controller/ServerController.java
@@ -4,44 +4,48 @@ package Controller;
  * ServerController handles the over all logic on the server side.
  * The class contains the main method that starts the program and makes it possible for a client
  * to obtain a connection by creating an instance of ServerNetwork.
- *
+ * <p>
  * version 1.0 2020-03-23
+ *
  * @autor Angelica Asplund, Emma Svensson and Theresa Dannberg
  */
 
 import Model.NetCommands;
+import Model.RegisteredUsers;
+import Model.User;
 
 import java.util.ArrayList;
 import java.util.concurrent.LinkedBlockingDeque;
 
 public class ServerController implements ClientListener {
 
+    private RegisteredUsers registeredUsers;
     private ServerNetwork network;
-    private LinkedBlockingDeque<ArrayList<Transferrable>> clientTask; //här läggs alla inkommande arraylists från klienterna.
+    private LinkedBlockingDeque<ArrayList<Transferrable>> clientTaskBuffer; //här läggs alla inkommande arraylists från klienterna.
 
     public ServerController() {
-        clientTask = new LinkedBlockingDeque<>();
+        registeredUsers = new RegisteredUsers();
+        clientTaskBuffer = new LinkedBlockingDeque<>();
         network = new ServerNetwork(this, 6583);
     }
-
 
     @Override
     public void sendList(ArrayList<Transferrable> message) {
         //lägg in listan i en buffer så att controllern kan hantera listan sen i egen tråd.
-        clientTask.add(message);
+        clientTaskBuffer.add(message);
     }
 
     public void handleClientTask() {
 
         // 1. Plocka ut första "uppgiften" från clientTask
         // 2. Plocka ut tex position[0]  (om där är enumet)
-        Transferrable command = clientTask.getFirst().get(0);
+        Transferrable command = clientTaskBuffer.getFirst().get(0);
 
         // 3. Skicka in enumet i en switch sats som kontrollerar vilket enum där är
         // 4. En metod per enum, namnen ska vara talande för vad som händer i de olika scenarion
         // 5. Skriv metoderna för de olika situationerna.
 
-        switch ((NetCommands)command) {
+        switch ((NetCommands) command) {
             case register:
                 //metodnamnfördetta(); ska skicka tillbaka registrationOk eller registrationDenied, detta räcker för första sprinten...
                 break;
@@ -51,6 +55,9 @@ public class ServerController implements ClientListener {
         }
     }
 
+    public void addRegisteredUser(User newUser){
+        registeredUsers.addRegisteredUser(newUser);
+    }
 
     public static void main(String[] args) {
         ServerController prog = new ServerController();
