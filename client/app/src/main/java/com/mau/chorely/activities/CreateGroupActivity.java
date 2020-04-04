b207410eb3b31f23dc22c04eb012b2253c50de49
Lull3n <61215087+Lull3n@users.noreply.github.com>
2020-04-04 16:10:44 +0200

    Started implementing group list.
    Started implementing group list. Not yet clickable.

--- client/app/src/main/java/com/mau/chorely/activities/CreateGroupActivity.java
+++ client/app/src/main/java/com/mau/chorely/activities/CreateGroupActivity.java
@@ -1,7 +1,9 @@
 package com.mau.chorely.activities;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.ListAdapter;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -12,22 +14,35 @@ import com.mau.chorely.model.Model;
 import com.mau.chorely.model.ModelInstances;
 
 import java.security.acl.Group;
+import java.util.ArrayList;
 
 import shared.transferable.NetCommands;
 import shared.transferable.RequestID;
 import shared.transferable.TransferList;
 
 public class CreateGroupActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_group);
-        ActionBar ab = getSupportActionBar();
-        // R.menu.mymenu is a reference to an xml file named mymenu.xml which should be inside your res/menu directory.
-        // If you don't have res/menu, just create a directory named "menu" inside res
 
 
+        ArrayList<ListItem> groupList = new ArrayList<>();
+        groupList.add(new ListItem(R.drawable.ic_group_black_24dp, "Blabla", "Blaaaslfkasflkasf"));
+        groupList.add(new ListItem(R.drawable.ic_group_black_24dp, "Blabla", "asdasdasd"));
+        groupList.add(new ListItem(R.drawable.ic_group_black_24dp, "Blabla", "Blaaaslfkasflkasf"));
+
+        mRecyclerView = findViewById(R.id.recyclerViewGroups);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mAdapter = new RecyclerViewAdapter(groupList);
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
 
     }
 
@@ -45,8 +60,6 @@ public class CreateGroupActivity extends AppCompatActivity {
 
     private void updateGroups(Group[] updatedGroups){
         if(updatedGroups != null){
-
-
 
 
 
