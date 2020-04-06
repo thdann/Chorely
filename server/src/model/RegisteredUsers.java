b66820979a5118fdfa2d4ac5f82822780f38cd9a
thdann <theresa_dannberg@hotmail.com>
2020-04-06 19:46:46 +0100

    Update in RegisteredUser

--- server/src/model/RegisteredUsers.java
+++ server/src/model/RegisteredUsers.java
@@ -2,31 +2,115 @@ package model;
 
 import shared.transferable.User;
 
-import java.io.ObjectInputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.util.ArrayList;
+
+/**
+ * RegisteredUser contains all the registered users by reading and writing to a file stored on the server.
+ * version 1.0 2020-04-06
+ *
+ * @author Theresa Dannberg
+ */
 
 public class RegisteredUsers {
 
     private String filename;
-    private OutputStream oos;
-    private ObjectInputStream ois;
+    private ArrayList<User> registeredUsers;
 
-    private ArrayList<User> registeredUsers = new ArrayList<>();
+    /**
+     * The constructor sets the path to the file with registered users.
+     * If the file doesn´t already exist it creates it.
+     */
 
-    public RegisteredUsers(String filename) {
-        this.filename = filename;
+    public RegisteredUsers() {
+        this.filename = "files/registeredUsers.dat";
+
+        File savedUsers = new File(filename);
+        if (savedUsers.exists()) {
+            readUsersFromFile();
+
+        } else {
+            registeredUsers = new ArrayList<>();
+            writeUsersToFile();
+
+        }
+
     }
+
+
+    public void addTestUsers() {
+        registeredUsers.clear();
+        registeredUsers.add(new User("Theresa", "mittPassword"));
+        registeredUsers.add(new User("Emma", "emmasPassword"));
+        registeredUsers.add(new User("Angelica", "angelicasPassword"));
+        registeredUsers.add(new User("Tim", "timsPassword"));
+        registeredUsers.add(new User("Fredrik", "fredrikPassword"));
+        System.out.printf("När det tillagt %d användare", registeredUsers.size());
+
+    }
+    public void printUsers() {
+        if (registeredUsers.size() == 0) {
+            System.out.println("Listan är tom på användare");
+        } else {
+            for (User u : registeredUsers) {
+                System.out.printf("Username: %s \tPassword: %s\n", u.getUsername(), u.getPassword());
+            }
+
+        }
+
+    }
+
+    /**
+     * TODO: no need for comment?
+     */
 
     public void addRegisteredUser(User newUser) {
         registeredUsers.add(newUser);
+
     }
 
-    public void setRegisteredUsers(ArrayList<User> registeredUsers) {
-        this.registeredUsers = registeredUsers;
+
+    /**
+     * TODO: no need for comment?
+     */
+
+    public void readUsersFromFile() {
+        try (ObjectInputStream ois = new ObjectInputStream(new BufferedInputStream(new FileInputStream(filename)))) {
+            registeredUsers = (ArrayList<User>) ois.readObject();
+
+        } catch (IOException | ClassNotFoundException e) {
+            e.getMessage();
+
+        }
+
     }
 
-    public ArrayList<User> getRegisteredUsers() {
-        return registeredUsers;
+    /**
+     * TODO: no need for comment?
+     */
+
+    public void writeUsersToFile() {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(filename)))) {
+            oos.writeObject(registeredUsers);
+            oos.flush();
+
+        } catch (IOException e) {
+            e.getMessage();
+
+        }
+
     }
+
+
+    public static void main(String[] args) {
+        RegisteredUsers test = new RegisteredUsers();
+        test.printUsers();
+        test.readUsersFromFile();
+        test.addTestUsers();
+        test.writeUsersToFile();
+        test.readUsersFromFile();
+        test.printUsers();
+
+    }
+
 }
