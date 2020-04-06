749c63f5a1e4572cbb2dce20bec9ad87985d911d
thdann <theresa_dannberg@hotmail.com>
2020-04-06 19:31:13 +0200

    Created streams in RegisteredUsers

--- server/src/model/RegisteredUsers.java
+++ server/src/model/RegisteredUsers.java
@@ -1,17 +1,32 @@
 package model;
 
-
-
 import shared.transferable.User;
 
+import java.io.ObjectInputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 
 public class RegisteredUsers {
 
+    private String filename;
+    private OutputStream oos;
+    private ObjectInputStream ois;
+
     private ArrayList<User> registeredUsers = new ArrayList<>();
+
+    public RegisteredUsers(String filename) {
+        this.filename = filename;
+    }
 
     public void addRegisteredUser(User newUser) {
         registeredUsers.add(newUser);
     }
 
+    public void setRegisteredUsers(ArrayList<User> registeredUsers) {
+        this.registeredUsers = registeredUsers;
+    }
+
+    public ArrayList<User> getRegisteredUsers() {
+        return registeredUsers;
+    }
 }
