7b60751421be88515c37e267ee99def21dc3c0d1
Lull3n <61215087+Lull3n@users.noreply.github.com>
2020-04-22 13:00:43 +0200

    Delete InvalidRequestIDException.java

--- client/app/src/main/java/com/mau/chorely/model/utils/InvalidRequestIDException.java
+++
@@ -1,7 +0,0 @@
-package com.mau.chorely.model.utils;
-
-public class InvalidRequestIDException extends Exception {
-    public InvalidRequestIDException(String message){
-        super(message);
-    }
-}
