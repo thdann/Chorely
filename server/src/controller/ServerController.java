80d614657bcf1e7d448e7301bf84f986a485f4b3
catarola <62558054+catarola@users.noreply.github.com>
2020-04-22 16:01:12 +0200

    Update ServerController.java

--- server/src/controller/ServerController.java
+++ server/src/controller/ServerController.java
@@ -48,12 +48,16 @@ public class ServerController implements ClientListener {
     }
 
     public void addOnlineClient(User user, ClientHandler client) {
-       User rebellUser = registeredUsers.getUserFromFile(user);
-       ArrayList<GenericID> groupMemberships = rebellUser.getGroups();
-       for(GenericID id: groupMemberships){
-       // Group group = registeredGroups.getGroupById();
-       }
+        User rebellUser = registeredUsers.getUserFromFile(user);
+        ArrayList<GenericID> groupMemberships = rebellUser.getGroups();
 
+        for (GenericID id : groupMemberships) {
+            Group group = registeredGroups.getGroupByID(id);
+            ArrayList<Transferable> data = new ArrayList<>();
+            data.add(group);
+            Message message = new Message(NetCommands.updateGroup, user, data);
+            sendReply(message);
+        }
 
         onlineClients.put(user, client);
 
