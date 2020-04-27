557a2cbb7a7847be6cf7384982368779e6ea1eee
thdann <theresa_dannberg@hotmail.com>
2020-04-27 15:14:18 +0200

    update layout groups

---
+++ client/app/src/main/java/com/mau/chorely/activities/EditChoreActivity.java
@@ -0,0 +1,16 @@
+package com.mau.chorely.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mau.chorely.R;
+
+public class EditChoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_chore);
+    }
+}
