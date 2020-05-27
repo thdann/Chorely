4dd07c49905d6494487182857f15840f2e7c3915
Fredrik Jeppsson <fredrik.m.jeppsson@gmail.com>
2020-05-27 12:13:52 +0200

    removed unused import in servercontroller

--- server/src/controller/ServerController.java
+++ server/src/controller/ServerController.java
@@ -7,7 +7,6 @@ import shared.transferable.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.LinkedBlockingQueue;
