051b9789f9d295cfce600a06b83f4a882ce1b046
JSalomonsson <johansalomonsson1@gmail.com>
2023-02-09 12:44:13 +0100

    Update ServerController.java

--- server/src/controller/ServerController.java
+++ server/src/controller/ServerController.java
@@ -144,15 +144,14 @@ public class ServerController {
     }
 
     /**
-     * @param msg The message containing the data of the users to receive notifications.
-     * @return
      * @Author Johan, Måns
      * Sends notifications to all users in the specified message's data.
      * The method loops through the data of the msg parameter and sends a notification to each user.
      * The NetCommands value of each notification is set to NetCommands.notificationReceived
      * and the data is set to the original data from msg.
+     * @param msg The message containing the data of the users to receive notifications.
      */
-    private Message sendNotifications(Message msg) {
+    private void sendNotifications(Message msg) {
         Message reply;
 
         if (msg.getData() != null) {
@@ -163,12 +162,10 @@ public class ServerController {
                     System.out.println(data.get(i));
                     reply = new Message(NetCommands.notificationReceived, (User) data.get(i), data);
                     sendReply(reply);
-                    return reply;
                 }
             }
         }
 
-        return null;
     }
 
     /**
