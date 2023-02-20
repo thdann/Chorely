e122965cde39eb45970564527ef1f261ab18a12d
keyjpg <93946569+keyjpg@users.noreply.github.com>
2023-02-20 11:38:59 +0100

    x

--- server/src/service/GroupQueries.java
+++ server/src/service/GroupQueries.java
@@ -17,9 +17,9 @@ public class GroupQueries {
     //add a group to the database
     public Group createGroup(String owner, String groupName, String description) {
         Group createdGroup = null;
-        owner =makeSqlSafe(owner);
-        groupName =makeSqlSafe(groupName);
-        description =makeSqlSafe(description);
+        owner = makeSqlSafe(owner);
+        groupName = makeSqlSafe(groupName);
+        description = makeSqlSafe(description);
         String query = "INSERT INTO [Group] (group_name, group_owner, group_description) VALUES ('" + groupName + "', '" + owner + "', '" + description + "');" +
                 "INSERT INTO [Member] VALUES ((select group_id from [Group] where group_name = '" + groupName + "' AND group_owner = '" + owner + "'), '" + owner + "', 0, 1);" +
                 "SELECT * FROM [Group] WHERE group_id = (select group_id from [Group] where group_name = '" + groupName + "' AND group_owner = '" + owner + "');";
@@ -40,14 +40,51 @@ public class GroupQueries {
 
     public Group getGroup(int groupID) {
         //todo return group with all attributes, chores, members and rewards
-        Group gotGroup = null;
-        return gotGroup;
+        Group group = null;
+        String query = "SELECT * FROM [Group] WHERE group_id = " + groupID + ";";
+
+        try {
+            ResultSet resultSet = queryExecutor.executeReadQuery(query);
+            resultSet.next();
+            group = new Group(
+                    Integer.parseInt(resultSet.getString("group_id")),
+                    resultSet.getString("group_name"),
+                    resultSet.getString("group_owner"),
+                    resultSet.getString("group_description"));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return group;
     }
 
-    public Group updateGroup(Group groupToUpdate) {
-        //todo return the group with updated
+    public Group updateGroupName(int id, String name) {
         Group updatedGroup = null;
-        return updatedGroup;
+
+        String query = "UPDATE [Group]\nSET group_name = '" + makeSqlSafe(name)
+                + "'\nWHERE group_id = " + id + ";";
+
+        try {
+            queryExecutor.executeUpdateQuery(query);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return updatedGroup = getGroup(id);
+    }
+
+    public Group updateGroupDescription(int id, String description) {
+        Group updatedGroup = null;
+
+        String query = "UPDATE [Group]\nSET group_description = '" + makeSqlSafe(description)
+                + "'\nWHERE group_id = " + id + ";";
+
+        try {
+            queryExecutor.executeUpdateQuery(query);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return updatedGroup = getGroup(id);
     }
 
     /**
